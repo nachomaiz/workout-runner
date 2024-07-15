18a43bb7ae84e7b4144602905c005db0cecce476
nachomaiz <ignacio.maiz@gmail.com>
2024-07-15 20:00:21 +0100

    main returns 0

--- workout/__main__.py
+++ workout/__main__.py
@@ -13,15 +13,17 @@ WIDTH = 1200
 FPS = 60
 
 
-def main():
+def main() -> int:
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     pg.display.set_caption("Workout")
     clock = pg.time.Clock()
     assets = Assets.from_path("assets/")
     sections = load_sections("assets/data/workout_data.json", assets)
-    app = App(sections, screen, clock, assets)
+    app = App(sections, screen, clock, assets, FPS)
 
-    return app.run()
+    app.run()
+
+    return 0
 
 
 if __name__ == "__main__":
